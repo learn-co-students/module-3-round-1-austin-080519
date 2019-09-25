a0e724cf73866cad25ff6c21ffd4416ac443c338
Jason <jas0nle@yahoo.com>
2019-09-25 12:25:26 -0500

    added my own comments

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const likeButton = document.getElementById("like_button");
 const submitButton = document.querySelector('[type="submit"]');
 const commentInput = document.getElementById("comment_input");
 
+// Function that fetches the image and all its properties and displays it on the DOM after page loads
 function fetchImage() {
   fetch(imageURL)
   .then(response => response.json())
@@ -31,6 +32,7 @@ function fetchImage() {
   })
 }
 
+// Function that updates likes in the backend
 function likeImage() {
   let data = {
     image_id: 3474
@@ -52,6 +54,7 @@ function likeImage() {
   })
 }
 
+// Function that creats a new comment in the backend and updates the DOM
 function createComment() {
   let data = {
     image_id: 3474,
@@ -76,11 +79,10 @@ function createComment() {
   })
 }
 
+// Function that deletes comment in the backend and updates the DOM
 function deleteComment(comment) {
   let data = {
-    id: comment['id'],
-    content: comment['content'],
-    image_id: comment['image_id']
+    id: comment['id']
   }
 
   let objConfig = {
@@ -95,11 +97,12 @@ function deleteComment(comment) {
   .then(response => response.json())
   .then(commentObj => {
     console.log(commentObj);
-    let commentItem = document.querySelector(`[comment-id="${comment['id']}"]`);
+    let commentItem = document.querySelector(`[comment-id="${comment['id']}"]`);  // Gets the comment that was deleted from databse
     commentItem.parentNode.removeChild(commentItem);
   })
 }
 
+// Callback function that displays all comments associated with image in a list
 function displayComments(image) {
   let comments = image['comments'];
 
@@ -108,39 +111,44 @@ function displayComments(image) {
     commentItem.setAttribute("comment-id", `${comment['id']}`);
     commentItem.innerText = comment['content'];
 
+    // *Bonus* Creating delete button 
     const deleteButton = document.createElement("button");
     deleteButton.setAttribute("comment-button-id", `${comment['id']}`);
     deleteButton.innerText = "Delete";
     createDeleteEvent(deleteButton, comment);
-
     commentItem.appendChild(deleteButton);
+
     commentsList.appendChild(commentItem);
   }
 }
 
+// Callback function that adds a newly created comment to the list
 function newComment(comment) {
   const commentItem = document.createElement("li");
   commentItem.setAttribute("comment-id", `${comment['id']}`);
   commentItem.innerText = commentInput.value;
   commentsList.appendChild(commentItem);
 
+  // *Bonus* Creating delete button 
   const deleteButton = document.createElement("button");
   deleteButton.setAttribute("comment-button-id", `${comment['id']}`);
   deleteButton.innerText = "Delete";
   createDeleteEvent(deleteButton, comment);
-
   commentItem.appendChild(deleteButton);
+
   commentsList.appendChild(commentItem);
 }
 
+
+// Function that will add an event listener every time a new delete button is created
 function createDeleteEvent(button, comment) {
   button.addEventListener("click", event => {
     event.preventDefault();
-    deleteComment(comment);
+    deleteComment(comment); 
   })
 }
 
-
+// Callback function that updates likes on DOM only
 function incrementLikes() {
   let currentLikes = totalLikes.innerText;
   parseInt(currentLikes);
@@ -150,11 +158,12 @@ function incrementLikes() {
 
 
 
-
+// I decided to declare all my variables and functions outside of the page load event
 document.addEventListener('DOMContentLoaded', () => {
   
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
+  // Calling fetchImage function after DOM has loaded to display the page
   fetchImage();
 
   // Like button event listener
